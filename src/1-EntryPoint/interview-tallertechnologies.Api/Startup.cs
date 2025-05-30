51dae903b9cfa8d826f9ab95f26d44ff8af698d1
CarlosViniciusVieira <viniciuslrd@gmail.com>
2025-05-30 19:21:56 -0300

    Added new classe to configuration services, and defined life cycle of instances. Added class UserRepository

---
+++ src/1-EntryPoint/interview-tallertechnologies.Api/Startup.cs
@@ -0,0 +1,29 @@
+﻿using interview_tallertechnologies.Application.UseCases.User.v1.Queries;
+using interview_tallertechnologies.Data.Repositories;
+using interview_tallertechnologies.Data.UnitOfwork;
+using interview_tallertechnologies.Domain.Interfaces;
+
+namespace interview_tallertechnologies.Api
+{
+    public static class Startup
+    {
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(GetUserByUserNameQueryProfile).Assembly);
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(GetUserByUserNameUseCase).Assembly);
+                cfg.Lifetime = ServiceLifetime.Scoped;
+            });
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+    }
+}
---
+++ src/3-Infrastructure/interview-tallertechnologies.Data/Repositories/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using interview_tallertechnologies.Data.Context;
+using interview_tallertechnologies.Domain.Entities;
+using interview_tallertechnologies.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace interview_tallertechnologies.Data.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        private readonly SqlDbContext _context;
+        private readonly ILogger<UserRepository> _logger;
+
+        public UserRepository(
+            SqlDbContext context,
+            ILogger<UserRepository> logger) : base(context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<User> GetByUsernameAsync(string username)
+        {
+            _logger.LogInformation($"[UserRepository].GetByUsernameAsync started for username: {username}");
+
+            try
+            {
+                var user = await _context.Users
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(u => u.Username == username);
+
+                _logger.LogInformation($"[UserRepository].GetByUsernameAsync completed for username: {username}");
+                return user;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"[UserRepository] Error in GetByUsernameAsync for username: {username}");
+                throw;
+            }
+        }
+    }
+}
